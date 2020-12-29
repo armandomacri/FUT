09f41481b901661b7348a961c54fcc0893a75fd7
mcasini <mirko.casini97@gmail.com>
2020-12-29 15:06:33 +0100

    modified player class (errors)

---
+++ FUTGui/src/main/java/org/unipi/group15/PlayerCardViewController.java
@@ -0,0 +1,127 @@
+package org.unipi.group15;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import user.UserSessionService;
+
+public class PlayerCardViewController {
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private Label userIdLabel;
+
+    @FXML
+    private TextField playerName;
+
+    @FXML
+    private TextField overall;
+
+    @FXML
+    private TextField position;
+
+    @FXML
+    private TextField paceStat;
+
+    @FXML
+    private TextField dribblingStat;
+
+    @FXML
+    private TextField passingStat;
+
+    @FXML
+    private TextField shootingStat;
+
+    @FXML
+    private TextField defendingStat;
+
+    @FXML
+    private TextField physicalityStat;
+
+    @FXML
+    private TextField extendedName;
+
+    @FXML
+    private TextField club;
+
+    @FXML
+    private TextField league;
+
+    @FXML
+    private TextField nationality;
+
+    @FXML
+    private TextField dateOfBirth;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField weight;
+
+    @FXML
+    private TextField quality;
+
+    @FXML
+    private TextField revision;
+
+    @FXML
+    private TextField origin;
+
+    @FXML
+    private TextField weakFoot;
+
+    @FXML
+    private TextField skillMoves;
+
+    @FXML
+    private TextField traits;
+
+    @FXML
+    private TextField prefFoot;
+
+    @FXML
+    private ProgressBar paceBar;
+
+    @FXML
+    private ProgressBar dribblingBar;
+
+    @FXML
+    private ProgressBar shootingBar;
+
+    @FXML
+    private ProgressBar defendingBar;
+
+    @FXML
+    private ProgressBar physicalityBar;
+
+    @FXML
+    private ProgressBar passingBar;
+
+    @FXML
+    private Button showComments;
+
+    @FXML
+    private void initialize(){
+        UserSessionService userSession = App.getSession();
+        usernameLabel.setText(userSession.getUsername());
+        userIdLabel.setText(userSession.getUserId());
+
+        // mostra tutti i campi della player card
+
+    }
+
+    @FXML
+    private void goToComments(){
+        // carica tutti i commenti del giocatore
+    }
+
+
+
+
+
+}
